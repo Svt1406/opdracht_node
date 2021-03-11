6abd0cfa2f90d81f499a5be5715d34598d75c777
Svt1406 <79196867+Svt1406@users.noreply.github.com>
2021-03-12 00:36:26 +0100

    Add files via upload

---
+++ opdracht_node/script.js
@@ -0,0 +1,7 @@
+console.log("hallo Winc Academy Studenten");
+// Dit is een grote som.
+/* let multiply = 1230941 * 1823794;
+console.log('multiply=',multiply);
+let divide = 637263 / 54;
+console.log('divide=',divide); */
+
